1d27a2d444ff76104f4c511e3b778bde1fb95963
A.Sergeev <aesergnsk@gmail.com>
2018-07-05 22:50:24 +0700

    Попытки создать контроллер

--- VKSearcher/Controllers/SearchController.cs
+++ VKSearcher/Controllers/SearchController.cs
@@ -1,18 +1,28 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
 using VKSearcher.Models;
 
 namespace VKSearcher.Controllers
 {
+    [RoutePrefix("api/search")]
     public class SearchController : ApiController
     {
-        public SearchController()
-        {
-            
-        }
-
-        [HttpGet]
+        [HttpGet, Route("")]
         public VKUserModel Get()
         {
+            var api = new VkApi();
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = "905e7fc0875abc172a2b01a0b8eb058df9423a146bc12476b7d86a93c57852886372c69680a125ee1a646"
+            });
+            var res = api.Groups.Get(new GroupsGetParams());
+
+            Console.WriteLine(res.TotalCount);
+
+            Console.ReadLine();
             return new VKUserModel();
         }
     }
